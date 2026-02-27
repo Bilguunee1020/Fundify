a99fbe774ed5eca2f36052605cd704c75c3bb826
25HP2782 25HP2782 <25HP2782@administrators-iMac.local>
2026-02-27 17:25:15 +0800

    Update proxy configuration

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: ["/api/admin/login"],
+});
 
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)"],
